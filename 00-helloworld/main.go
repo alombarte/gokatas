037fbc98c45273d5dae2000268f12266b2502b38
Albert Lombarte <alombarte@gmail.com>
2015-08-27 21:29:57 +0200

    Playing with functions and maps

--- 00-helloworld/main.go
+++ 00-helloworld/main.go
@@ -8,11 +8,11 @@ import (
 	"math"
 	"math/rand"
 	"runtime"
+	"strings"
 	"time"
-	// Remote libraries
 )
 
-const Pi = 3.14
+const Pi = 3.14159265358979323846264338327950288419716939937510582097494459
 
 // Declaring and initialiting several vars:
 var c, python, golang bool = false, false, true
@@ -30,7 +30,9 @@ func (v *Vertex) Abs() float64 {
 
 // getOS returns the current operattive system on which the program runs.
 func getOS() string {
+
 	currentOS := ""
+
 	switch os := runtime.GOOS; os {
 	case "darwin":
 		currentOS = "OS X"
@@ -44,15 +46,48 @@ func getOS() string {
 	return currentOS
 }
 
+// swapText returns 2 strings
+func swapText(x, y string) (string, string) {
+	return y, x
+}
+
+// WordRepetition returns a map with the number of occurrences of each word inside a string.
+func WordRepetition(s string) map[string]int {
+
+	// Map declaration:
+	var repetitions map[string]int
+	repetitions = make(map[string]int) // After declaring a map needs to be initialized with 'make' before using it.
+
+	words := strings.Fields(s)
+	
+	// Iterate using key => value in a range. Underscore means the var won't be used.
+	for _, word := range words {
+		repetitions[word]++
+	}
+
+	return repetitions
+}
+
 func main() {
 
-	fmt.Printf("hello, world\n")
-	fmt.Println("The time is", time.Now())
-	fmt.Println("My favorite number is", rand.Intn(10)) // Caution: deterministic
+	// Mission accomplished:
+	fmt.Println(swapText("world", "hello"))
+	fmt.Println("---")
+
+	// Printing: https://golang.org/pkg/fmt/
+	fmt.Printf("π=%.5f (type %T).\n", Pi, Pi) // Print 5 decimals and variable type
+	fmt.Println("The time is:", time.Now())
+	fmt.Println("My favorite number is always:", rand.Intn(10)) // Note: deterministic operation
 	fmt.Println("Running under:", getOS())
 	fmt.Println("---")
 
 	v := Vertex{3.0, -4.0}
 	fmt.Println("Vertex:", v.X, v.Y, "Abs():", v.Abs())
+
+	fmt.Println("---")
+	a := "Am Zehnten Zehnten um zehn Uhr zehn zogen zehn zahme Ziegen zehn Zentner Zucker zum Zoo"
+	fmt.Printf("Word repetition for string:\n%q\n", a)
+	fmt.Println(WordRepetition(a))
+	fmt.Println("---")
 
 }
