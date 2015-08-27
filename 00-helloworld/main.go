521222b0aa392d4c7ca69b71dde160812bfa0e55
Albert Lombarte <alombarte@gmail.com>
2015-08-27 20:35:48 +0200

    Methods on structs

--- 00-helloworld/main.go
+++ 00-helloworld/main.go
@@ -5,6 +5,7 @@ package main
 // Including libraries
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"runtime"
 	"time"
@@ -22,6 +23,12 @@ type Vertex struct {
 	Y float64
 }
 
+// Methods on structs. This is like Classes on other languages: V.abs()
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// getOS returns the current operattive system on which the program runs.
 func getOS() string {
 	currentOS := ""
 	switch os := runtime.GOOS; os {
@@ -36,9 +43,16 @@ func getOS() string {
 
 	return currentOS
 }
+
 func main() {
+
 	fmt.Printf("hello, world\n")
 	fmt.Println("The time is", time.Now())
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("My favorite number is", rand.Intn(10)) // Caution: deterministic
 	fmt.Println("Running under:", getOS())
+	fmt.Println("---")
+
+	v := Vertex{3.0, -4.0}
+	fmt.Println("Vertex:", v.X, v.Y, "Abs():", v.Abs())
+
 }
